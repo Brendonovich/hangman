3eb15dd05603f66379a69cd782848cddb8f43056
Brendan Allan <brendonovich@outlook.com>
2021-04-23 03:13:38 +0800

    use secret key in prod

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -16,19 +16,19 @@ use Mix.Config
 #   url: database_url,
 #   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
-# secret_key_base =
-#   System.get_env("SECRET_KEY_BASE") ||
-#     raise """
-#     environment variable SECRET_KEY_BASE is missing.
-#     You can generate one by calling: mix phx.gen.secret
-#     """
+secret_key_base =
+  System.get_env("SECRET_KEY_BASE") ||
+    raise """
+    environment variable SECRET_KEY_BASE is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
 
 config :hangman, HangmanWeb.Endpoint,
   http: [
     port: String.to_integer(System.get_env("PORT") || "4000"),
     transport_options: [socket_opts: [:inet6]]
   ],
-  # secret_key_base: secret_key_base
+  secret_key_base: secret_key_base
 
 # ## Using releases (Elixir v1.9+)
 #
