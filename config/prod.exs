e303dda1cce6da12b4275897e9edfae42546fed8
Brendan Allan <brendonovich@outlook.com>
2021-04-23 03:48:43 +0800

    remove manifest from prod config

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,6 @@ use Mix.Config
 # before starting your production server.
 config :hangman, HangmanWeb.Endpoint,
   url: [host: System.get_env("HOSTNAME"), port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
