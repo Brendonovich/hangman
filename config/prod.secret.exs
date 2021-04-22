5bcdf1cafb26c7235b80052172a6df49fe0d2f66
Brendan Allan <brendonovich@outlook.com>
2021-04-23 03:11:52 +0800

    remove secret key requirement

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -28,7 +28,7 @@ config :hangman, HangmanWeb.Endpoint,
     port: String.to_integer(System.get_env("PORT") || "4000"),
     transport_options: [socket_opts: [:inet6]]
   ],
-  secret_key_base: secret_key_base
+  # secret_key_base: secret_key_base
 
 # ## Using releases (Elixir v1.9+)
 #
